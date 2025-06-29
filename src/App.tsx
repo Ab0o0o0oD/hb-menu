babb3eeb70b3c8f57b81f3e084577897dfb8b4a9
Abdulrazak Kanjo <abdulrazak.kanjo@gjensidige.no>
2025-06-30 00:07:57 +0200

    fix: update base url

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,33 @@ import pizzaImage from './assets/pizza-menu.jpg'
 import main from './assets/main.jpg'
 import frokostMenu from './assets/frokost-menu.jpg'
 import shawarmaMenu from './assets/shawarma-menu.jpg'
+import {Tabs} from "@/components/Tabs.tsx";
 
 
 function App() {
+    const tableItemss = [
+        {name: "Pizza", anchor: "#pizza"},
+        {name: "Shawarma", anchor: "#shawarma"},
+        {name: "Frokost", anchor: "#frokost"}
+    ];
+    return (
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center ">
+            <div className="w-full max-w-sm md:max-w-3xl">
+                <Tabs tabItems={tableItemss}/>
+                <img src={main} alt={'pizza-menu'}/>
+                <div id="shawarma">
+                    <img src={shawarmaMenu} alt={'shawarma-menu'}/>
+                </div>
+                <div id="pizza">
+                    <img src={pizzaImage} alt={'pizza-menu'}/>
+                </div>
+                <div id="frokost">
+                    <img src={frokostMenu} alt={'pizza-menu'}/>
+                </div>
+            </div>
 
-  return (
-      <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-          <div className="w-full max-w-sm md:max-w-3xl">
-              <img src={main} alt={'pizza-menu'}/>
-              <img src={shawarmaMenu} alt={'pizza-menu'}/>
-              <img src={pizzaImage} alt={'pizza-menu'}/>
-              <img src={frokostMenu} alt={'pizza-menu'}/>
-          </div>
-      </div>
-  )
+        </div>
+    )
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/hb-menu/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
