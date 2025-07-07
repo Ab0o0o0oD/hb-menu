8edeb7bc3bd460d2a99afb1d0bca6d113c0c08cf
Abdulrazak Kanjo <abdulrazak.kanjo@gjensidige.no>
2025-07-07 10:24:00 +0200

    fix: Update page tab title

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
                     <div
                         className={"w-full md:w-1/2 md:p-10 items-center flex justify-center text-black text-center sm:py-4"}>
                         <div className='col-span-6'>
-                            <h1 className="text-4xl lg:text-5xl font-semibold mb-5 text-black dark:text-white md:4px lg:text-start text-center">
+                            <h1 className="text-4xl lg:text-5xl font-semibold mb-5 text-black/80 dark:text-white md:4px lg:text-start text-center">
                                 Welcome to Hungry Birds
                             </h1>
                             <p className='text-black/55 dark:text-white/50 lg:text-lg font-normal mb-10 lg:text-start text-center'>
@@ -28,7 +28,7 @@ function App() {
                                 passion!</p>
                             <div className='md:flex align-middle justify-center lg:justify-end'>
                                 <a href='#menu-section'
-                                   className='flex border w-full md:w-auto mt-5 md:mt-0 border-primary justify-center rounded-full text-xl font-medium items-center py-5 px-10 text-primary hover:text-white hover:bg-primary'>
+                                   className='flex border w-full md:w-auto mt-5 md:mt-0 border-primary justify-center rounded-full text-xl font-medium items-center py-5 px-10 text-black hover:text-white hover:bg-primary'>
                                     Explore our menu</a>
                             </div>
                         </div>
